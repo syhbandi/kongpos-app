4244c958c2c437682089bd6c345a24937f813d25
Hana Ramdhani <hanawesave@gmail.com>
2022-06-24 16:19:58 +0800

    up

--- src/pages/laporan/PenjualanPerperiode.jsx
+++ src/pages/laporan/PenjualanPerperiode.jsx
@@ -22,7 +22,7 @@ const PenjualanPerdivisi = () => {
 
   const [src, setSearch] = useState([]);
 
-  const [formErrors, setFormErrors] = useState({});
+  const [formErrors, setFormErrors] = useState();
 
   const [isSubmit, setIsSubmit] = useState(false);
 
@@ -93,7 +93,7 @@ const PenjualanPerdivisi = () => {
       awal: formValues.awal ? formValues.awal : dateNow,
       akhir: formValues.akhir ? formValues.akhir : dateNow,
       jenis: "1",
-      search: src != "" ? src : "",
+      search: src ? src : "",
       order_col: orderColumn != "" ? orderColumn : "",
       order_type: typeOrder != "" ? typeOrder : "",
       limit: currentPage * formValues.rec,
@@ -116,7 +116,7 @@ const PenjualanPerdivisi = () => {
     console.log(formValues);
   };
   const hidden = (e) => {
-    setFormErrors(validateHidden(formValues));
+    setFormErrors(validateHidden(src));
   };
 
   function submit(e) {
@@ -175,6 +175,8 @@ const PenjualanPerdivisi = () => {
       formValues.rec,
       "1"
     );
+    setFormErrors(validate(src));
+    setIsSubmit(true);
   }
 
   const OrderCol = (order_col, type) => {
@@ -185,7 +187,7 @@ const PenjualanPerdivisi = () => {
       formValues.awal ? formValues.awal : dateNow,
       formValues.akhir ? formValues.akhir : dateNow,
       "1",
-      "",
+      src ? src : "",
       orderColumn,
       typeOrder,
       "0",
@@ -197,7 +199,7 @@ const PenjualanPerdivisi = () => {
       formValues.awal ? formValues.awal : dateNow,
       formValues.akhir ? formValues.akhir : dateNow,
       "1",
-      "",
+      src ? src : "",
       orderColumn,
       typeOrder,
       "0",
@@ -207,29 +209,29 @@ const PenjualanPerdivisi = () => {
   };
 
   const validate = (values) => {
-    const errors = {};
-    if (!values.awal) {
-      errors.awal = "Tidak boleh kosong";
+    let errors = "";
+    if (!values) {
+      errors = "Tidak boleh kosong";
     }
-    if (!values.akhir) {
-      errors.akhir = "Tidak boleh kosong";
-    }
-    if (!values.rec) {
-      errors.rec = "Tidak boleh kosong";
-    }
+    // if (!values.akhir) {
+    //   errors.akhir = "Tidak boleh kosong";
+    // }
+    // if (!values.rec) {
+    //   errors.rec = "Tidak boleh kosong";
+    // }
     return errors;
   };
   const validateHidden = (values) => {
-    const errors = {};
-    if (values.awal) {
-      errors.awal = "";
+    let errors = "";
+    if (values) {
+      errors = "";
     }
-    if (values.akhir) {
-      errors.akhir = "";
-    }
-    if (values.rec) {
-      errors.rec = "";
-    }
+    // if (values.akhir) {
+    //   errors.akhir = "";
+    // }
+    // if (values.rec) {
+    //   errors.rec = "";
+    // }
     return errors;
   };
   const defaultDate = () => {
@@ -265,7 +267,7 @@ const PenjualanPerdivisi = () => {
     // );
   });
 
-  document.addEventListener("DOMContentLoaded", function (event) {});
+  // document.addEventListener("DOMContentLoaded", function (event) {});
 
   return (
     <div className="rounded-lg bg-white shadow-lg p-5">
@@ -294,7 +296,7 @@ const PenjualanPerdivisi = () => {
                     value={formValues.awal ? formValues.awal : dateNow}
                     // onBlur={hidden}
                   ></input>
-                  <p className="text-xs text-red-600">{formErrors.awal}</p>
+                  {/* <p className="text-xs text-red-600">{formErrors.awal}</p> */}
                 </div>
               </div>
             </div>
@@ -319,7 +321,7 @@ const PenjualanPerdivisi = () => {
                     value={formValues.akhir ? formValues.akhir : dateNow}
                     // onBlur={hidden}
                   ></input>
-                  <p className="text-xs text-red-600">{formErrors.akhir}</p>
+                  {/* <p className="text-xs text-red-600">{formErrors.akhir}</p> */}
                 </div>
               </div>
             </div>
@@ -350,7 +352,7 @@ const PenjualanPerdivisi = () => {
                     <option value="50">50</option>
                     <option value="100">100</option>
                   </select>
-                  <p className="text-xs text-red-600">{formErrors.rec}</p>
+                  {/* <p className="text-xs text-red-600">{formErrors.rec}</p> */}
                 </div>
               </div>
             </div>
@@ -385,10 +387,12 @@ const PenjualanPerdivisi = () => {
             type="text"
             onChange={(e) => setSearch(e.target.value)}
             value={src}
+            onBlur={hidden}
             id="simple-search"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search"
           ></input>
+          <p className="text-xs text-red-600">{formErrors}</p>
         </div>
         <button
           type="submit"
@@ -557,6 +561,9 @@ const PenjualanPercustomer = () => {
 
   const handleGet = () => {
     dispatch(getPenjualan(formData));
+    const newFormData = { ...formData };
+    newFormData.count_stats = 1;
+    dispatch(getPenjualanCount(newFormData));
   };
 
   const handleGetCount = () => {
@@ -574,7 +581,7 @@ const PenjualanPercustomer = () => {
 
   return (
     <>
-      <div className="bg-white rounded-lg shadow-lg w-full p-4">
+      <div className="rounded-lg bg-white shadow-lg p-5">
         <button
           className="bg-blue-600 rounded-lg px-4 py-3 text-white font-medium"
           onClick={handleGet}
@@ -582,14 +589,138 @@ const PenjualanPercustomer = () => {
         >
           Get data
         </button>
-        <button
+        {/* <button
           className="bg-blue-600 rounded-lg px-4 py-3 text-white font-medium"
           onClick={handleGetCount}
         >
           Get data
-        </button>
-      </div>
-      <div className="mt-5 bg-white rounded-lg shadow-lg w-full p-4">
+        </button> */}
+        {/* Laporan Penjualan Perdivisi */}
+        {/* <pre>{JSON.stringify(formValues, undefined, 2)}</pre> */}
+        {/* <form onSubmit={(e) => submit(e)}>
+          <div className="p-0 bg-white">
+            
+            <div className="grid grid-cols-4">
+              <div className="p-0 bg-white rounded-md flex items-center justify-left">
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-14">
+                    <label
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      htmlFor="inline-full-name"
+                    >
+                      Awal
+                    </label>
+                  </div>
+                  <div className="md:w-40">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="date"
+                      name="awal"
+                      onChange={handleChange}
+                      value={formValues.awal ? formValues.awal : dateNow}
+                      // onBlur={hidden}
+                    ></input>
+                    <p className="text-xs text-red-600">{formErrors.awal}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-0 bg-white rounded-md flex items-center justify-left">
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-14">
+                    <label
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      htmlFor="inline-full-name"
+                    >
+                      Akhir
+                    </label>
+                  </div>
+                  <div className="md:w-40">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="date"
+                      name="akhir"
+                      onChange={handleChange}
+                      value={formValues.akhir ? formValues.akhir : dateNow}
+                    ></input>
+                    <p className="text-xs text-red-600">{formErrors.akhir}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-0 bg-white rounded-md flex items-center justify-left">
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-40">
+                    <label
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      htmlFor="inline-full-name"
+                    >
+                      Pilih Jumlah Baris
+                    </label>
+                  </div>
+                  <div className="md:w-28">
+                    <label htmlFor="underline_select" className="sr-only">
+                      Underline select
+                    </label>
+                    <select
+                      id="underline_select"
+                      name="rec"
+                      onChange={handleChange}
+                      value={formValues.rec}
+                      // onClick={hidden}
+                      className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                    >
+                      <option value="10">10</option>
+                      <option value="25">25</option>
+                      <option value="50">50</option>
+                      <option value="100">100</option>
+                    </select>
+                    <p className="text-xs text-red-600">{formErrors.rec}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-0 bg-white rounded-md flex items-center justify-left">
+                <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <button
+                      className="p-2 bg-stone-400 rounded-lg"
+                      type="submit"
+                    >
+                      <i className={faDotCircle}></i>
+                      Filter
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+        <form className="w-80 flex items-center" onSubmit={(e) => Search(e)}>
+          <label htmlFor="simple-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              cari
+            </div>
+            <input
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              value={src}
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search"
+            ></input>
+          </div>
+          <button
+            type="submit"
+            className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            cari
+          </button>
+        </form> */}
+
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-lg">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -704,8 +835,49 @@ const PenjualanPercustomer = () => {
               ))}
           </tbody>
         </table>
-        <strong>Jumlah Data : {dataCount && dataCount.jumlah_record}</strong>
+        <div className="p-0 bg-white">
+          <div className="grid grid-cols-2">
+            <div className="p-0 bg-white rounded-md flex items-center justify-left ">
+              <strong>
+                <strong>Jumlah Data : {dataCount}</strong>
+              </strong>
+            </div>
+            <div className="p-0 bg-white rounded-md flex items-center justify-end">
+              {/* <ReactPaginate
+                previousLabel={"<<"}
+                nextLabel={">>"}
+                breakLabel={"..."}
+                pageCount={
+                  jmlRecord && jmlRecord.jumlah_record / formValues.rec
+                }
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={3}
+                onPageChange={handlePageClick}
+                containerClassName={"inline-flex -space-x-px "}
+                pageClassName={"inline-flex -space-x-px"}
+                pageLinkClassName={
+                  "py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                }
+                previousClassName={
+                  "py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                }
+                nextClassName={
+                  "py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                }
+                breakClassName={"inline-flex -space-x-px"}
+                breakLinkClassName={
+                  "py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                }
+                activeClassName="border border-cyan-300 dark:bg-gray-500 rounded-lg"
+                activeLinkClassName="border border-cyan-300 dark:bg-gray-500 rounded-lg"
+              /> */}
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div className="bg-white rounded-lg shadow-lg w-full p-4"></div>
+      <div className="mt-5 bg-white rounded-lg shadow-lg w-full p-4"></div>
     </>
   );
 };
