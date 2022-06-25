8a47de0b184b5127c550a34e4c52db8050abf83c
Ahmad Syahbandi <saboonlbk@gmail.com>
2022-06-25 21:21:25 +0800

    table 90%

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,59 +6,31 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 
-const Table = ({}) => {
+const Table = ({ data }) => {
   const [order, setOrder] = useState({
     field: "",
     asc: true,
   });
-  const data = [
-    {
-      no: 1,
-      nama: "hana",
-      alamat: "dompu",
-      hobi: "membaca",
-    },
-    {
-      no: 2,
-      nama: "hana2",
-      alamat: "dompu2",
-      hobi: "insekyur",
-    },
-    {
-      no: 3,
-      nama: "hana3",
-      alamat: "dompu3",
-      hobi: "bersepeda",
-    },
-  ];
-  const headers = data.length
-    ? Object.keys(data[0]).map((value) => value)
-    : ["Galat"];
+  const headers = Object.keys(data[0]).map((value) => value);
 
   const handleColClick = (value) => {
-    if (!data.length) {
-      return;
-    }
-
-    const newOrder = order;
-
-    if (newOrder.field === value) {
-      if (!newOrder.asc) {
-        Object.assign(newOrder, { field: "", asc: true });
+    if (order.field === value) {
+      if (!order.asc) {
+        setOrder({ ...order, field: "", asc: true });
       } else {
-        Object.assign(newOrder, { ...newOrder, asc: false });
+        setOrder({ ...order, asc: false });
       }
     } else {
-      newOrder.field = value;
+      setOrder({ ...order, field: value });
     }
   };
 
   return (
-    <table className="w-full border-collapse border-2">
+    <table className="w-full border-collapse">
       <thead className="bg-white">
-        <tr className="border-1">
+        <tr>
           {headers.map((value, index) => (
-            <th key={index} className="border-2">
+            <th key={index} className="border border-gray-500">
               <button
                 className="flex items-center justify-between w-full px-4 py-3"
                 id={value}
@@ -80,26 +52,18 @@ const Table = ({}) => {
         </tr>
       </thead>
       <tbody>
-        {data.length ? (
-          data.map((result, index) => (
-            <tr key={index}>
-              {headers.map((value, index) => (
-                <td className="bg-white px-4 py-3 border-2" key={index}>
-                  {result[value]}
-                </td>
-              ))}
-            </tr>
-          ))
-        ) : (
-          <tr>
-            <td
-              className="bg-white px-4 py-3 border-2"
-              colSpan={headers.length}
-            >
-              Tidak ada data
-            </td>
+        {data.map((result, index) => (
+          <tr key={index}>
+            {headers.map((value, index) => (
+              <td
+                className="bg-white px-4 py-3 border border-gray-500"
+                key={index}
+              >
+                {result[value]}
+              </td>
+            ))}
           </tr>
-        )}
+        ))}
       </tbody>
     </table>
   );
