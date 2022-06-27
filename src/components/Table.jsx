9bc97f3bba1f29d0acf3974433c14ba9b4068289
Ahmad Syahbandi <saboonlbk@gmail.com>
2022-06-27 15:15:34 +0800

    update component table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,5 @@
 import {
+  faSearch,
   faSort,
   faSortAmountDown,
   faSortAmountDownAlt,
@@ -7,8 +8,23 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
-const Table = ({ data, handleSort, colSort, typeSort, offset }) => {
+const Table = ({
+  data,
+  handleSort,
+  colSort,
+  typeSort,
+  offset,
+  length,
+  search,
+  handlePage,
+  handleFilter,
+  handleFormChange,
+  dataCount,
+  handlePaginate,
+  page,
+}) => {
   const [order, setOrder] = useState({
     col: "",
     type: "",
@@ -35,52 +51,128 @@ const Table = ({ data, handleSort, colSort, typeSort, offset }) => {
   }, [colSort, typeSort]);
 
   return (
-    <table className="w-full border-collapse">
-      <thead className="bg-white">
-        <tr>
-          <th className="border border-gray-500">NO</th>
-          {headers.map((value, index) => (
-            <th key={index} className="border border-gray-500">
-              <button
-                className="flex items-center justify-between w-full px-4 py-3"
-                id={value}
-                onClick={() => handleColClick(value)}
-              >
-                <span className="uppercase font-bold">{value}</span>
-                <FontAwesomeIcon
-                  icon={
-                    order.col.slice(1, order.col.length - 1) === value
-                      ? order.type === ""
-                        ? faSortAmountDownAlt
-                        : faSortAmountDown
-                      : faSort
-                  }
-                  className={`${
-                    order.col.slice(1, order.col.length - 1) === value
-                      ? "text-black"
-                      : "text-gray-300"
-                  }`}
-                />
-              </button>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((result, index) => (
-          <tr key={index} className="hover:bg-gray-100 bg-white cursor-pointer">
-            <td className="px-4 py-3 border border-gray-500 text-center">
-              {index + offset + 1}
-            </td>
+    <>
+      {/* jumlah data per page */}
+      <div className="flex flex-col md:flex-row items-center justify-between mb-3">
+        <div className="flex items-center gap-5 mb-3 md:mb-0">
+          <span>Menampilkan</span>
+          <select
+            id="length"
+            onChange={handlePage}
+            value={length}
+            className="outline-none border border-gray-500 px-3 py-2 rounded-lg cursor-pointer"
+          >
+            {[10, 25, 50, 100].map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+          <span>data</span>
+        </div>
+        {/* pencarian */}
+        <form onSubmit={handleFilter}>
+          <div className="flex items-center rounded-lg">
+            <input
+              type="text"
+              id="search"
+              placeholder="Cari"
+              className="w-full md:w-56 outline-0 px-3 py-2 border border-gray-500 rounded-l-lg"
+              onChange={handleFormChange}
+              value={search}
+            />
+            <button
+              type="submit"
+              className="p-3 rounded-r-lg bg-blue-700 border border-blue-700 text-white flex items-center hover:bg-blue-900 hover:border-blue-900"
+            >
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+        </form>
+      </div>
+      <table className="w-full border-collapse">
+        <thead className="bg-white">
+          <tr>
+            <th className="border border-gray-500">NO</th>
             {headers.map((value, index) => (
-              <td className=" px-4 py-3 border border-gray-500" key={index}>
-                {result[value]}
-              </td>
+              <th key={index} className="border border-gray-500">
+                <button
+                  className="flex items-center justify-between w-full px-4 py-3"
+                  id={value}
+                  onClick={() => handleColClick(value)}
+                >
+                  <span className="uppercase font-bold">{value}</span>
+                  <FontAwesomeIcon
+                    icon={
+                      order.col.slice(1, order.col.length - 1) === value
+                        ? order.type === ""
+                          ? faSortAmountDownAlt
+                          : faSortAmountDown
+                        : faSort
+                    }
+                    className={`${
+                      order.col.slice(1, order.col.length - 1) === value
+                        ? "text-black"
+                        : "text-gray-300"
+                    }`}
+                  />
+                </button>
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((result, index) => (
+            <tr
+              key={index}
+              className="hover:bg-gray-100 bg-white cursor-pointer"
+            >
+              <td className="px-4 py-3 border border-gray-500 text-center">
+                {index + offset + 1}
+              </td>
+              {headers.map((value, index) => (
+                <td className=" px-4 py-3 border border-gray-500" key={index}>
+                  {result[value]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* footer */}
+      <div className="flex flex-col md:flex-row items-center mt-5">
+        <div className="font-bold">Total data: {dataCount}</div>
+        <div className="ml-auto">
+          <ReactPaginate
+            forcePage={page}
+            previousLabel={"<<"}
+            nextLabel={">>"}
+            breakLabel={"..."}
+            pageCount={Math.ceil(dataCount && dataCount / length)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePaginate}
+            containerClassName={
+              "inline-flex items-center select-none border-collapse"
+            }
+            pageLinkClassName={
+              "p-3 bg-white font-medium border-y border-gray-500 text-blue-700 hover:bg-gray-200"
+            }
+            previousLinkClassName={
+              "rounded-l-lg p-3 bg-white font-medium border-y border-l border-gray-500 text-blue-700 hover:bg-gray-200"
+            }
+            nextLinkClassName={
+              "p-3 bg-white font-medium border-y border-r border-gray-500 text-blue-700 rounded-r-lg hover:bg-gray-200"
+            }
+            breakLinkClassName={
+              "p-3 bg-white font-medium border-y border-gray-500 text-blue-700 hover:bg-gray-200"
+            }
+            activeLinkClassName="bg-blue-700 text-yellow-400 hover:bg-blue-700"
+            renderOnZeroPageCount={null}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/laporan/Penjualan.jsx
+++ src/pages/laporan/Penjualan.jsx
@@ -144,45 +144,6 @@ const Penjualan = ({ jenis }) => {
         </button>
       </div>
       <div className="rounded-lg bg-white shadow-lg p-5 min-h-[200px]">
-        {/* jumlah data per page */}
-        <div className="flex flex-col md:flex-row items-center justify-between mb-3">
-          <div className="flex items-center gap-5 mb-3 md:mb-0">
-            <span>Menampilkan</span>
-            <select
-              id="length"
-              onChange={handlePage}
-              value={formData["length"]}
-              className="outline-none border border-gray-500 px-3 py-2 rounded-lg cursor-pointer"
-            >
-              {[10, 25, 50, 100].map((value, index) => (
-                <option key={index} value={value}>
-                  {value}
-                </option>
-              ))}
-            </select>
-            <span>data</span>
-          </div>
-          {/* pencarian */}
-          <form onSubmit={handleFilter}>
-            <div className="flex items-center rounded-lg">
-              <input
-                type="text"
-                id="search"
-                placeholder="Cari"
-                className="w-full md:w-56 outline-0 px-3 py-2 border border-gray-500 rounded-l-lg"
-                onChange={handleFormChange}
-                value={formData.search}
-              />
-              <button
-                type="submit"
-                className="p-3 rounded-r-lg bg-blue-700 border border-blue-700 text-white flex items-center hover:bg-blue-900 hover:border-blue-900"
-              >
-                <FontAwesomeIcon icon={faSearch} />
-              </button>
-            </div>
-          </form>
-        </div>
-
         {/* table data */}
         {data && data.length > 0 ? (
           <Table
@@ -191,6 +152,14 @@ const Penjualan = ({ jenis }) => {
             colSort={formData.order_col}
             typeSort={formData.order_type}
             offset={formData.limit}
+            length={formData.length}
+            search={formData.search}
+            handlePage={handlePage}
+            handleFilter={handleFilter}
+            handleFormChange={handleFormChange}
+            dataCount={dataCount}
+            handlePaginate={handlePaginate}
+            page={page}
           />
         ) : (
           <div className="w-full text-center mt-5">
@@ -200,40 +169,6 @@ const Penjualan = ({ jenis }) => {
             <div>Tidak mendapatkan data</div>
           </div>
         )}
-
-        {/* footer */}
-        <div className="flex flex-col md:flex-row items-center mt-5">
-          <div className="font-bold">Total data: {dataCount}</div>
-          <div className="ml-auto">
-            <ReactPaginate
-              forcePage={page}
-              previousLabel={"<<"}
-              nextLabel={">>"}
-              breakLabel={"..."}
-              pageCount={Math.ceil(dataCount && dataCount / formData.length)}
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={3}
-              onPageChange={handlePaginate}
-              containerClassName={
-                "inline-flex items-center select-none border-collapse"
-              }
-              pageLinkClassName={
-                "p-3 bg-white font-medium border-y border-gray-500 text-blue-700 hover:bg-gray-200"
-              }
-              previousLinkClassName={
-                "rounded-l-lg p-3 bg-white font-medium border-y border-l border-gray-500 text-blue-700 hover:bg-gray-200"
-              }
-              nextLinkClassName={
-                "p-3 bg-white font-medium border-y border-r border-gray-500 text-blue-700 rounded-r-lg hover:bg-gray-200"
-              }
-              breakLinkClassName={
-                "p-3 bg-white font-medium border-y border-gray-500 text-blue-700 hover:bg-gray-200"
-              }
-              activeLinkClassName="bg-blue-700 text-yellow-400 hover:bg-blue-700"
-              renderOnZeroPageCount={null}
-            />
-          </div>
-        </div>
       </div>
 
       {/* loader */}
